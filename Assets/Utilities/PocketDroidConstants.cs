77dd62b7fd54f646e67549fceca79a09893859de
Isaac Besora Vilardaga <isaac.besora@gmail.com>
2018-06-19 16:27:06 +0200

    Working UI

--- Assets/Utilities/PocketDroidConstants.cs
+++ Assets/Utilities/PocketDroidConstants.cs
@@ -8,5 +8,6 @@ public class PocketDroidConstants : MonoBehaviour {
 	public static string SCENE_CAPTURE = "Capture"; 
 
 	public static string TAG_DROID = "Droid";
+	public static string TAG_OVERRIDE_ORB = "Override Orb";
 	
 }
