e750bdba89e4f455a2b3d8ab842735f5c3a39a9e
Isaac Besora Vilardaga <isaac.besora@gmail.com>
2018-06-19 13:39:54 +0200

    Capture scene

---
+++ Assets/Scenes/capture/CaptureSceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureSceneManager : PocketDroidsSceneManager {
+
+	public override void playerTapped(GameObject player) {
+
+	}
+
+	public override void droidTapped(GameObject droid) {
+		
+	}
+}
